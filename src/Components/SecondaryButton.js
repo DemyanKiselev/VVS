a299348834dc542d48696c575eacdbb9d8efeb77
DemyanKiselev <demyan-kiselev@yandex.ru>
2022-12-30 20:09:27 +0300

    Change colour

--- src/Components/SecondaryButton.js
+++ src/Components/SecondaryButton.js
@@ -5,18 +5,18 @@ function SecondaryButton(props) {
   // Здесь устанавливается минимальная ширина для кнопки
   let minWidth = props.minWidth === undefined ? "217px" : props.minWidth;
   const Button = styled.div`
-    /* background: #ffffff; */
+    /* background: #dfdfdf; */
     border: 1px solid #64748b; /* цвет обводки */
     border-radius: 24px; /*закругление*/
     /*box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);*/
     min-width: ${minWidth};
     color: #152842;
     &:hover {
-      border-color: #ffffff;
+      border-color: #dfdfdf;
       opacity: 0.8; /* прозрачность на 80% при ховере */
     }
     &:active {
-      background: #ffffff;
+      background: #dfdfdf;
     }
   `;
   return (
